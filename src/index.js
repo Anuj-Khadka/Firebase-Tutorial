02816b8d9f127a2f8e99a8c4a0deb68662335411
Anuj-Khadka <anujkhadka008@gmail.com>
2023-05-06 19:59:13 +0545

    firebase lesson-3

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("hello from lesson 1")
+import {initializeApp} from "firebase/app"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBOPCvr1Nl--enQIE3yCew6u3A0vyKiRos",
+  authDomain: "dont-firebase.firebaseapp.com",
+  projectId: "dont-firebase",
+  storageBucket: "dont-firebase.appspot.com",
+  messagingSenderId: "946471568465",
+  appId: "1:946471568465:web:ed53877ccee90ee54ea470",
+  measurementId: "G-XH4W37X5NW"
+};
+
+initializeApp(firebaseConfig)
